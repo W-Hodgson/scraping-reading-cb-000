6dac2e2034b591e3d84572ba5e79b8f5d3a14886
Will Hodgson <will.hodgson@current-rms.com>
2019-08-21 10:36:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,4 +3,6 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
-puts doc.css(".headline-26OIBN").text.strip
+# puts doc.css(".headline-26OIBN").text.strip
+
+doc.css(".tout__label.heading.heading--level-4")
