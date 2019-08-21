c46fc5fcc46609b5d33efabfa431030ff440ab00
Will Hodgson <will.hodgson@current-rms.com>
2019-08-21 10:36:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,3 +6,5 @@ doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 # puts doc.css(".headline-26OIBN").text.strip
 
 courses = doc.css(".tout__label.heading.heading--level-4")
+
+courses.each do 
